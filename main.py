997400322b6d790a42c7eed3bce3a35c7d1a0806
Atul Anand <atulcleaver@gmail.com>
2022-01-14 14:14:28 +0530

    Added the madlibs code

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+noun_plu = input("Plural noun: ")
+place = input("Place: ")
+noun = input("Noun: ")
+noun_plu_1 = input("Plural noun: ")
+noun_1 = input("Noun: ")
+adjective = input("Adjective: ")
+verb = input("Verb: ")
+numbers = input("Number: ")
+adjective_1 = input("Adjective: ")
+body_part = input("Body part: ")
+verb_1 = input("Verb: ")
 
+
+print(f'\nTwo {noun_plu}, both alike in dignity,'
+      f'\nIn fair {place}, where we lay our scene,'
+      f'\nFrom ancient {noun} break to new mutiny,'
+      f'\nWhere civil blood makes civil hands unclean.'
+      f'\nFrom forth the fatal loins of these two foes'
+      f'\nA pair of star-cross`d {noun_plu_1} take their life;'
+      f'\nWhole misadventured piteous overthrows'
+      f'\nDo with their {noun_1} bury their parents` strife.'
+      f'\nThe fearful passage of their {adjective} love,'
+      f'\nAnd the continuance of their parents` rage,'
+      f'\nWhich, but their children`s end, nought could {verb},'
+      f'\nIs now the {numbers} hours` traffic of our stage;'
+      f'\nThe which if you with {adjective_1} {body_part} attend,'
+      f'\nWhat here shall {verb_1}, our toil shall strive to mend.')
